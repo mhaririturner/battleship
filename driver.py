b644d54366876dd72a0f0064447ea4afc08b58b2
Max Hariri-Turner <maxkht8@gmail.com>
2021-11-19 15:29:26 -0800

    boilerplate code

---
+++ driver.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python
+
+"""
+driver.py: does stuff
+"""
+
+__author__ = "Max Hariri-Turner"
+__email__ = "maxkht8@gmail.com"
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
